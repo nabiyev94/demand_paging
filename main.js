631092ae1f31559bc009fbfb263599675a16795c
nabiyev94 <81377046+nabiyev94@users.noreply.github.com>
2021-11-26 19:47:30 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-var slider_img = document.querySelector('.w');
-var linkPages = [
-  '<div class="w"><a href="https://www.youtube.com/channel/UCX6b17PVsYBQ0ip5gyeme-Q" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/en/e/e9/Crash_Course_logo.png"></a></div>', 
-  '<div class="w"><a href="https://www.youtube.com/"><img src="https://i0.wp.com/blog.indiefolio.com/wp-content/uploads/2017/08/YTLogo_old_new_1680-1.gif?fit=800%2C600&ssl=1" heigth="92" width="92"></a></div>', 
-  '<div class="w"><a href="https://uspace.univie.ac.at/"><img src="https://spl-ksa.univie.ac.at/fileadmin/user_upload/s_spl24/uspace_logo.png" width="150" height="150"></a></div>'];
+var my_image = document.querySelector('.my_image');
+var images = ['https://upload.wikimedia.org/wikipedia/en/e/e9/Crash_Course_logo.png', 
+	      'https://i0.wp.com/blog.indiefolio.com/wp-content/uploads/2017/08/YTLogo_old_new_1680-1.gif?fit=800%2C600&ssl=1',
+	      'https://static.facebook.com/images/whatsapp/www/whatsapp-promo.png', 
+	      'https://miro.medium.com/max/1000/1*VsJQWtP7CO7J-lFdNpDF1g.png" width="120', 
+	      'https://spl-ksa.univie.ac.at/fileadmin/user_upload/s_spl24/uspace_logo.png" width="150" height="150'];
 var i = 0;
 
 function prev(){
-	if(i <= 0) i = linkPages.length;	
+	if(i <= 0) i = images.length;	
 	i--;
-	return setPage();			 
+	return setImg();			 
 }
 
 function next(){
-	if(i >= linkPages.length-1) i = -1;
+	if(i >= images.length-1) i = -1;
 	i++;
-	return setPage();			 
+	return setImg();			 
 }
 
-function setPage(){
-	return w.setAttribute('class', linkpages[i]);
+function setImg(){
+	return my_image.setAttribute('src', images[i]);
 	
 }
