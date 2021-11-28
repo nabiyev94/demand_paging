52c7a92dfa1c85a1bf9b1b7b34fb05ff9b415c6c
nabiyev94 <81377046+nabiyev94@users.noreply.github.com>
2021-11-27 16:59:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var my_image = document.querySelector('.my_image');
 var images = ['https://i0.wp.com/blog.indiefolio.com/wp-content/uploads/2017/08/YTLogo_old_new_1680-1.gif?fit=800%2C600&ssl=1',
 	      'https://upload.wikimedia.org/wikipedia/en/e/e9/Crash_Course_logo.png',
 	      'https://static.facebook.com/images/whatsapp/www/whatsapp-promo.png', 
-	      'https://miro.medium.com/max/1000/1*VsJQWtP7CO7J-lFdNpDF1g.png" width="120', 
-	      /*'https://spl-ksa.univie.ac.at/fileadmin/user_upload/s_spl24/uspace_logo.png" width="150" height="150',
+	      'https://miro.medium.com/max/1000/1*VsJQWtP7CO7J-lFdNpDF1g.png', 
+	      /*'https://spl-ksa.univie.ac.at/fileadmin/user_upload/s_spl24/uspace_logo.png',
 	      'https://www.googlewatchblog.de/wp-content/uploads/google-translate-logo.jpg'.
 	      'http://blog.univie.ac.at/relaunch/wp-content/uploads/2016/07/ufind-1-300x130.png',
 	      'https://www.thinkwithgoogle.com/_qs/static/img/global/twg-logo-large.jpg',
